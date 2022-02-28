293cfc5191c80b82662ab626a6b21fee9a8dae42
Darren Wang <hdwang@berkeley.edu>
2022-02-27 22:53:48 -0800

    finished

--- hw4/P2Pattern.java
+++ hw4/P2Pattern.java
@@ -4,18 +4,21 @@
 
 public class P2Pattern {
     /* Pattern to match a valid date of the form MM/DD/YYYY. Eg: 9/22/2019 */
-    public static String P1 = ""; //FIXME: Add your regex here
+    public static String P1
+            = "(0?\\d|1[0-2])\\/([0-2]?\\d|3[01])\\/\\d\\d\\d\\d";
 
     /** Pattern to match 61b notation for literal IntLists. */
-    public static String P2 = ""; //FIXME: Add your regex here
+    public static String P2 = "(.\\s*\\d+)+.";
 
     /* Pattern to match a valid domain name. Eg: www.support.facebook-login.com */
-    public static String P3 = ""; //FIXME: Add your regex here
+    public static String P3
+            = "([a-z]+\\.)?([a-z]+(\\-[a-z]+|[a-z*])*\\.)+([a-z]{2,6})";
 
     /* Pattern to match a valid java variable name. Eg: _child13$ */
-    public static String P4 = ""; //FIXME: Add your regex here
+    public static String P4 = "[^\\d]+[a-z\\d_$]*";
 
     /* Pattern to match a valid IPv4 address. Eg: 127.0.0.1 */
-    public static String P5 = "";; //FIXME: Add your regex here
+    public static String P5
+            = "(([0-1]?\\d?\\d|2[0-4]\\d|25[0-5])\\.){3}([0-1]?\\d?\\d|2[0-4]\\d|25[0-5])";
 
 }
--- hw4/ReadInts.java
+++ hw4/ReadInts.java
@@ -34,7 +34,7 @@ public class ReadInts {
         Scanner s = new Scanner(input);
         while (s.hasNext()) {
             int nextInt = s.nextInt();
-            // FIXME
+            L.add(nextInt);
         }
         return L;
     }
@@ -48,7 +48,16 @@ public class ReadInts {
      *  from Scanner. Is there a way to skip over non-integers, instead of
      *  trying to read them and getting an exception? */
     public static List<Integer> smartReadInts(String input) {
-        // FIXME
-        return null;
+        ArrayList<Integer> L = new ArrayList<>();
+        Scanner s = new Scanner(input);
+        while (s.hasNext()) {
+            if (s.hasNextInt()) {
+                int nextInt = s.nextInt();
+                L.add(nextInt);
+            } else {
+                s.next();
+            }
+        }
+        return L;
     }
 }
